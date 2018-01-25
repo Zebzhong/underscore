7681d6bbb10ca2ec53079d9b14ac2ecd7a2f0875
zebzhong <bin_zhong@163.com>
2018-01-25 22:10:14 +0800

    熟悉对象和集合方法

---
+++ collection.js
@@ -0,0 +1,86 @@
+_.each = _.forEach = function(obj,iteratee,context){
+  iteratee = optimizeCb(iteratee,context);
+  var i,length;
+  if(isArrayLike(obj)){
+    for(i = 0,length = obj.length;i<length;i++){
+      iteratee(obj[i],i,obj);
+    }
+  }else{
+    var keys = _.keys(obj);
+    for(i = 0,length = keys.length;i<length;i++){
+      var key = keys[i];
+      iteratee(obj[key],key,obj);
+    }
+  }
+  return obj;
+}
+
+_.map = _.collect = function(obj,iteratee,context){
+  iteratee = cb(iteratee,context);
+  var keys = !isArrayLike(obj) && _.keys(obj),
+      length = (keys || obj).length,
+      results = [];
+  for(var i = 0;i<length;i++){
+    var currentKey = keys ? keys[i] : i;
+    results[i] = iteratee(obj[currentKey],currentKey,obj);
+  }
+  return results;
+}
+
+function createReduce(dir){
+  function iterator(obj, iteratee, memo, keys, index, length){
+    for(;index<length && index >=0;index+=dir){
+      var currentKey = keys ? keys[index] : index;
+      memo = iteratee(memo,obj[currentKey],currentKey,obj);
+    }
+    return memo;
+  }
+  return function(obj,iteratee,memo,context){
+    iteratee = optimizeCb(iteratee,context,4);
+    var keys = !isArrayLike(obj) && _.keys(obj),
+        length = (keys || obj).length,
+        index = dir > 0 ? 0 : length-1; 
+    if(arguments.length < 3){
+      memo = obj[keys ? keys[index]:index];
+      index += dir;
+    }    
+    return iterator(obj,iteratee,memo,keys,index,length);
+  }
+}
+
+function createReduce(dir){
+  function iterator(obj, iteratee, memo, keys, index, length){
+    for(;index < length && index >= 0;index+=dir){
+      var currentKey = keys ? keys[index] : index;
+      memo = iteratee(obj[currentKey],currentKey,obj);
+    }
+    return memo;
+  }
+  return function(obj,iteratee,memo,context){
+    iteratee = optimizeCb(iteratee,context);
+    var keys = !isArrayLike(obj) && _.keys(obj),
+        length = (keys || obj).length,
+        index = dir > 0 ? 0 : length-1;
+    if(arguments.length < 3){
+      memo = obj[keys ? keys[index] : index];
+      index += dir;
+    }
+    return  iteratee(obj,iteratee,memo,keys,index,length);
+  }
+}
+
+_.reduce = _.foldl = _.inject = createReduce(1);
+
+_.reduceRight = _.foldr = createReduce(-1);
+
+_.find = _.detect = function(obj,predicate,context){
+  var key;
+  if(isArrayLike(obj)){
+    key = _.findIndex(obj,predicate,context);
+  }else{
+    key = _.findKey(obj,predicate,context);
+  }
+  if(key !== void 0 && key !== -1)return obj[key];
+}
+
+
--- object.js
+++ object.js
@@ -36,6 +36,18 @@ _.keys = function(obj){
   return keys;
 }
 
+_.keys = function(obj){
+  if(!_.isObject(obj)) return [];
+  if(nativeKeys)return nativeKeys(obj);
+  var keys = [];
+  for(var key in obj){
+    if(_.has(obj,key)){
+      keys.push(key);
+    }
+  }
+  if(hasEnumBug)collectNonEnumProps(obj,keys);
+  return keys;
+}
 _.allKeys = function(obj){
   if(!_.isObject(obj))return [];
   var keys = [];
@@ -45,7 +57,15 @@ _.allKeys = function(obj){
   if(hasEnumBug)collectNonEnumProps(obj,keys);
   return keys;
 }
-
+_.allKeys = function(obj){
+  if(!_.isObject(obj))return [];
+  var keys = [];
+  for(var key in obj){
+    keys.push(key);
+  }
+  if(hasEnumBug)collectNonEnumProps(obj,keys);
+  return keys;
+}
 _.values = function(obj){
   var keys = _.keys(obj);
   var length = keys.length;
@@ -56,6 +76,15 @@ _.values = function(obj){
   return values; 
 }
 
+_.values = function(obj){
+  var keys = _.keys(obj);
+  var length = keys.length;
+  var values = Array(length);
+  for(var i = 0;i<length;i++){
+    values[i] = obj[keys[i]];
+  }
+  return values;
+}
 _.mapObject = function(obj,iteratee,context){
   iteratee = cb(iteratee,context);
   var keys = _.keys(obj),
@@ -69,6 +98,18 @@ _.mapObject = function(obj,iteratee,context){
   return results;
 }
 
+_.mapObject = function(obj,iteratee,context){
+  iteratee = cb(iteratee,context);
+  var keys = _.keys(obj),
+      length = keys.length,
+      results = {},
+      currentKey;
+  for(var i = 0;i<length;i++){
+    currentKey = keys[i];
+    results[currentKey] = iteratee(obj[currentKey],currentKey,obj);
+  }
+  return results;
+}
 _.pairs = function(obj){
   var keys = _.keys(obj);
   var length = keys.length;
@@ -79,6 +120,15 @@ _.pairs = function(obj){
   return pairs;
 }
 
+_.pairs = function(obj){
+  var keys = _.keys(obj);
+  var length = keys.length;
+  var pairs = [];
+  for(var i = 0;i<length;i++){
+    pairs[i] = [keys[i],obj[keys[i]]];
+  }
+  return pairs;
+}
 _.invert = function(obj){
   var result = {};
   var keys = _.keys(obj);
@@ -87,7 +137,14 @@ _.invert = function(obj){
   }
   return result;
 }
-
+_.invert = function(obj){
+  var result = {};
+  var keys = _.keys(obj);
+  for(var i = 0,length = keys.length;i<length;i++){
+    result[obj[keys[i]]] = keys[i];
+  }
+  return result;
+}
 _.functions = _.methods = function(obj){
   var names = [];
   for(var key in obj){
@@ -95,7 +152,13 @@ _.functions = _.methods = function(obj){
   }
   return names.sort();
 }
-
+_.functions = _.methods = function(obj){
+  var names = [];
+  for(var key in obj){
+    if(_.isFunction(obj))names.push(key);
+  }
+  return names.sort();
+}
 _.extend = createAssigner(_.allKeys);
 
 _.extendOwn = _.assign = createAssigner(_.keys);
@@ -107,6 +170,146 @@ _.findKey = function(obj,predicate,context){
     key = keys[i];
     if(predicate(obj[key],key,obj))return key;
   }
+}
+
+_.findKey = function(obj,predicate,context){
+  predicate = cb(predicate,context);
+  var keys = _keys(obj),key;
+  for(var i = 0,length = keys.length;i<lenght;i++){
+    keys = keys[i];
+    if(predicate(obj[key],key,obj))return key;
+  }
+}
+
+_.pick = function(object,oiteratee,context){
+  var result = {},
+      obj = object,
+      iteratee,keys;
+  if(obj == null)return result;
+  if(_.isFunction(oiteratee)){
+    keys = _.allKeys(obj);
+    iteratee = optimizeCb(oiteratee,context);
+  }else{
+    keys = flatten(arguments,false,false,1);
+    iteratee = function(value,key,obj){
+      return key in obj;
+    }
+    obj = Object(obj);
+  }    
+  for (var i = 0, length = keys.length; i < length; i++) {
+    var key = keys[i];
+    var value = obj[key];
+    // 满足条件
+    if (iteratee(value, key, obj)) result[key] = value;
+  }
+  return result;
+}
+
+_.omit = function(obj, iteratee, context) {
+  if (_.isFunction(iteratee)) {
+    iteratee = _.negate(iteratee);
+  } else {
+    var keys = _.map(flatten(arguments, false, false, 1), String);
+    iteratee = function(value, key) {
+      return !_.contains(keys, key);
+    };
+  }
+  return _.pick(obj, iteratee, context);
+};
+
+_.defaults = createAssigner(_.allKeys, true);
+
+_.create = function(prototype,props){
+  var result = baseCreate(prototype);
+  if(props)_.extendOwn(result,props);
+  return result;
+}
+
+_.tap = function(obj,interceptor){
+  interceptor(obj);
+  return obj;
+}
+
+_.isMatch = function(object,attrs){
+  var keys = _.keys(attrs),length = keys.length;
+  if(object == null)return !length;
+  var obj = Object(object);
+  for(var i = 0;i<length;i++){
+    var key = keys[i];
+    if(attrs[key] !== obj[key] || !(key in obj))return false;
+  }
+  return true;
+}
+
+_.clone = function(obj){
+  if(!_.isObject(obj))return obj;
+  return _.isArray(obj) ? obj.slice():_.extend({},obj);
+}
+
+var eq = function(a, b, aStack, bStack) {
+  if (a === b) return a !== 0 || 1 / a === 1 / b;
+  if (a == null || b == null) return a === b;
+  if (a instanceof _) a = a._wrapped;
+  if (b instanceof _) b = b._wrapped;
+  var className = toString.call(a);
+  if (className !== toString.call(b)) return false;
+  switch (className) {
+    case '[object RegExp]':
+    case '[object String]':
+      return '' + a === '' + b;
+    case '[object Number]':
+      if (+a !== +a) return +b !== +b;
+      return +a === 0 ? 1 / +a === 1 / b : +a === +b;
+    case '[object Date]':
+    case '[object Boolean]':
+      return +a === +b;
+  }
+
+  var areArrays = className === '[object Array]';
+  if (!areArrays) {
+    if (typeof a != 'object' || typeof b != 'object') return false;
+    var aCtor = a.constructor, bCtor = b.constructor;
+    if (aCtor !== bCtor && !(_.isFunction(aCtor) && aCtor instanceof aCtor &&
+        _.isFunction(bCtor) && bCtor instanceof bCtor) && ('constructor' in a && 'constructor' in b)) {
+      return false;
+    }
+  }
+  aStack = aStack || [];
+  bStack = bStack || [];
+  var length = aStack.length;
+  while (length--) {
+    if (aStack[length] === a) return bStack[length] === b;
+  }
+  aStack.push(a);
+  bStack.push(b);
+  if (areArrays) {
+    length = a.length;
+    if (length !== b.length) return false;
+    while (length--) {
+      if (!eq(a[length], b[length], aStack, bStack)) return false;
+    }
+  } else {
+    var keys = _.keys(a), key;
+    length = keys.length;
+    if (_.keys(b).length !== length) return false;
+    while (length--) {
+      key = keys[length];
+      if (!(_.has(b, key) && eq(a[key], b[key], aStack, bStack))) return false;
+    }
+  }
+  aStack.pop();
+  bStack.pop();
+  return true;
+};
+
+_.isEqual = function(a,b){
+  return eq(a,b);
+}
+
+_.isEmpty = function(obj){
+  if(obj == null)return true;
+  if(isArrayLike(obj) && (_.isArray(obj) || _.isString(obj) ||_.isArguments(obj))) return obj.length === 0;
+  return _.keys(obj).length === 0;
 }
 
 _.isArray = nativeIsArray || function(obj) {
